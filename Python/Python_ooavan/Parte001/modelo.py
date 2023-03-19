09ba919f678d9df2f6fea0146b60d13509eb8e74
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-03-19 12:03:01 -0300

    Dia 19/03

--- Python/Python_ooavan/Parte001/modelo.py
+++ Python/Python_ooavan/Parte001/modelo.py
@@ -1,5 +1,26 @@
 class filmes():
-    def __init__(self, nome, ano, duracao):
+    def __init__(self, nome, ano, duracao, likes):
+        self.nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
+class series():
+    def __init__(self, nome, ano, temporadas):
         self.nome = nome
-        self.nome = ano
-        self.ano = duracao
+        self.ano = ano
+        self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
+
+filme  = filmes('Senhor dos aneis com o resto errado', 2018, 160, 18)
+filme.dar_like()
+filme.dar_like()
+print(f'Nome do filme: {filme.nome}, Ano: {filme.ano}, Duração: {filme.duracao}, Likes: {filme.likes}')
+    
