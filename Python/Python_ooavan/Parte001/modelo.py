add2f21f4511fe6b3f10e3f2c3bf91f0a37e5242
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-03-19 17:05:26 -0300

    Dia 19/03

--- Python/Python_ooavan/Parte001/modelo.py
+++ Python/Python_ooavan/Parte001/modelo.py
@@ -1,26 +1,57 @@
 class filmes():
     def __init__(self, nome, ano, duracao, likes):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def novo_nome(self,novo_nome):
+        self.__nome = novo_nome.title()
 
 class series():
-    def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+    def __init__(self, nome, ano, temporadas, likes):
+        self.__nome = nome
         self.ano = ano
         self.temporadas = temporadas
-        self.likes = 0
+        self.__likes = 0
 
     def dar_like(self):
         self.likes += 1
 
+    @property
+    def likes(self):
+        return self.__likes
 
-filme  = filmes('Senhor dos aneis com o resto errado', 2018, 160, 18)
-filme.dar_like()
-filme.dar_like()
-print(f'Nome do filme: {filme.nome}, Ano: {filme.ano}, Duração: {filme.duracao}, Likes: {filme.likes}')
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
     
+    @nome.setter
+    def novo_nome(self,novo_nome):
+        self.__nome = novo_nome.title()
+
+filme  = filmes('Senhor dos aneis com o resto errado', 2018, 160, 0)
+serie = series('Outlander', 2017, 50, 0)
+filme.dar_like()
+filme.dar_like()
+serie.dar_like()
+serie.dar_like()
+serie.dar_like()
+serie.dar_like()
+print(f'Nome do filme: {filme.nome}, Ano: {filme.ano}, Duração: {filme.duracao}, Likes: {filme.likes}')
+print(f'Nome da serie: {serie.nome}, Ano: {serie.ano}, Duração: {serie.temporadas}, Likes: {serie.likes}')
