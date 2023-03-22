d46c0ff974486ab14479ddbfe410711bccdb6203
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-03-22 10:53:21 -0300

    Aula 22/03

--- Python/Python_ooavan/Parte004/modelo.py
+++ Python/Python_ooavan/Parte004/modelo.py
@@ -34,9 +34,20 @@ class series(Programa):
     
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} -  {self._likes}'
+    
+class Playlist(Programa):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
 
+    def tamanho(self):
+        return len(self.programas)
+    
 filme  = filmes('Senhor dos aneis com o resto errado', 2018, 160, 0)
 serie = series('Outlander', 2017, 7, 0)
+tmep = filmes('Tudo mundo lele da cuca', 2012, 160, 0)
+cuba = series('Serie do Jorge', 2023, 20, 0)
+
 filme.dar_like()
 filme.dar_like()
 serie.dar_like()
@@ -45,9 +56,11 @@ serie.dar_like()
 serie.dar_like()
 
 
-filmes_e_series = [filme, serie]
+filmes_e_series = [filme, serie, tmep, cuba]
+musicas_fim_de_semana = Playlist('Festa', filmes_e_series)
 
-for programa in filmes_e_series:
+
+for programa in musicas_fim_de_semana:
     print(programa)
 
     
