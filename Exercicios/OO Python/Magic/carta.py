66eb3632a7693da00c30bfe0ecf83259cb26ae2f
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-04-19 21:07:37 -0300

    Dia 19/04

--- Exercicios/OO Python/Magic/carta.py
+++ Exercicios/OO Python/Magic/carta.py
@@ -1,20 +1,28 @@
 class Jogador:
-    def __init__(self, name_player, hp):
-        self.name_player = name_player
+    def __init__(self, player, hp):
+        self.player = player
         self.hp = hp
 
 class Carta: 
-    def __init__(self, name, mana, color):
+    def __init__(self, name, mana,effect ,color):
         self.name = name
         self.mana = mana
+        self.effect = effect
         self.color = color
         
-    def Jogar(self):
+    def jogada(self):
+        self.jogada = True
         print(f'A {self.name} foi jogada!.')
 
+    def ativar_efeito(self):
+        if self.jogada:
+            print(f'O efeito da carta {self.name} foi ativado, seguintes efeitos: {self.effect}.')
+        else:
+            print(f'A carta {self.nome} não pode ser ativada pois ela ainda não foi usada.')
+
 class Criatura:
-    def __init__(self, name, mana, color, strength, toughness):
-        super().__init__(name, mana, color, 'Criatura')
+    def __init__(self, name, mana, color, player, strength, toughness):
+        super().__init__(name, mana, color, player, 'Criatura')
         self.strength = strength
         self.toughness = toughness
 
@@ -22,7 +30,7 @@ class Criatura:
         print(f'A {self.name} atacou com força de {self.strength}.')
 
     def atacar_jogador(self):
-        print(f'A {self.name} atacou o {}')
+        print(f'A {self.name} atacou o {self.player}')
 
     def bloquear(self):
         print(f'')
