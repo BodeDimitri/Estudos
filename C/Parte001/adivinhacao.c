8f92aeca2e709fa8760de310d241a5f1007a51a7
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-08-16 17:14:58 -0300

    Dia 16/08

--- C/Parte001/adivinhacao.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf('*************************************************');
-    printf('E a 3 vez que eu faço um jogo de adivinhação, PQP');
-    printf('*************************************************');
-}
