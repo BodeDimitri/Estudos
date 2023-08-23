9fb58fbe6caa55fdfd2637725a3f19686f2f61b7
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-08-23 12:19:17 -0300

    Dia 23/08

---
+++ JS/Back-end/JS_1/boolean2.js
@@ -0,0 +1,25 @@
+//bolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy ou falsy
+
+//0  => false
+//1 => true
+
+console.log(0 == false);
+console.log(0 == '');
+console.log(1 == true);
+
+//udefined
+//null => vazio
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
---
+++ JS/Back-end/JS_1/variables.js
@@ -0,0 +1,34 @@
+//var
+
+//var alt = 10;
+//var comprimento = 7;
+
+//area = alt * comprimento;
+//console.log(area);
+//var area
+
+//let forma = 'retangulo';
+//let alt = 10;
+//let comprimento = 7;
+//let area;
+
+//if (forma === 'retangulo') {
+//    area = alt * comprimento;
+//} else {
+//    area = (alt * comprimento) / 2;
+//}
+
+//console.log(area)
+
+const forma = 'triangulo';
+const alt = 5;
+const comprimento = 10;
+let area;
+
+if (forma === 'quadrado') {
+    area = alt * comprimento
+} else {
+    area = (alt * comprimento) / 2;
+}
+
+console.log(area)
