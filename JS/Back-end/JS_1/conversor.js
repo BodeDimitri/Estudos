a295a8516f6fa0e7c77e2735cdfe00ddb51ff2ff
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-08-23 15:50:50 -0300

    Dia 23/08

---
+++ JS/Back-end/JS_1/conversor.js
@@ -0,0 +1,24 @@
+//tipo de dados
+//booleanos
+
+//conversão implícita
+
+const numero = 456;
+const numeroString = '456';
+
+console.log(numero == numeroString);
+
+//Number() Converte a string passada para um numero
+//String() Converte o numero passado para uma string
+
+const numero2 = 456;
+const numeroString2 = '456';
+
+console.log(numero2 + Number(numeroString2));
+
+//conversão explícita
+
+const numero1 = 456;
+const numeroString1 = '456';
+
+console.log(numero1 === numeroString1);
