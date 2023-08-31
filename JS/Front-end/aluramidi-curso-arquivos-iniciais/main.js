dfebe15a055a5872a1b18b0000f246718aa5a58f
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-08-31 18:52:22 -0300

    Dia 31/08

---
+++ JS/Front-end/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,15 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+ listaDeTeclas[0].onclick = tocaSomPom;
+ listaDeTeclas[1].onclick = tocaSomPom;
+ listaDeTeclas[2].onclick = tocaSomPom;
+ listaDeTeclas[3].onclick = tocaSomPom;
+ listaDeTeclas[4].onclick = tocaSomPom;
+ listaDeTeclas[5].onclick = tocaSomPom;
+ listaDeTeclas[6].onclick = tocaSomPom;
+ listaDeTeclas[7].onclick = tocaSomPom;
+ listaDeTeclas[8].onclick = tocaSomPom;
