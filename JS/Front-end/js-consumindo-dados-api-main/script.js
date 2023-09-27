fe2626a9e8f0987feedb6a742b162bb087dcbfa2
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-09-27 09:41:10 -0300

    Dia 27/09

--- JS/Front-end/js-consumindo-dados-api-main/script.js
+++ JS/Front-end/js-consumindo-dados-api-main/script.js
@@ -1,3 +1,12 @@
 const consultaCep = fetch('https://viacep.com.br/ws/01001000/json')
+    .then(converte => converte.json())
+    .then(resposta =>{
+        if (resposta.erro) {
+            throw Error('Esse cep não existe')
+        } else
+        console.log(resposta)})
+
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('Processamento concluído'))
 
 console.log(consultaCep)
