6627e833dc6e5dfd45f642816caeff0d081f6007
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-09-28 10:37:22 -0300

    Dia 28/09

---
+++ JS/Back-end/JS_6/2708-node-lib-md-arquivos-iniciais/index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+//const chalk = require('chalk')
+
+console.log(chalk.blue('Oi'));
+
+console.log('olá mundo');
--- JS/Front-end/js-consumindo-dados-api-main/script1.js
+++ JS/Front-end/js-consumindo-dados-api-main/script1.js
@@ -1,4 +1,6 @@
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = '';
     try {
         const consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json`)
         var consultaCepConvertida = await consultaCep.json()
@@ -6,9 +8,18 @@ async function buscaEndereco(cep) {
         if (consultaCepConvertida.erro) {
             throw Error('CEP não existente')
         }
+        var cidade = document.getElementById('cidade')
+        var logradouro = document.getElementById('endereco')
+        var estado = document.getElementById('estado')
+
+        cidade.value = consultaCepConvertida.localidade;
+        logradouro.value = consultaCepConvertida.logradouro;
+        estado.value = consultaCepConvertida.uf;
+
         console.log(consultaCepConvertida)
         return consultaCepConvertida
         } catch (erro) {
+            mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
             console.log(erro)
         }
     }
