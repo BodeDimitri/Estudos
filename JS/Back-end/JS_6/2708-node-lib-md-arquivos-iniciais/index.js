efc9c3915ced522aca37a2db7f208490d53f20a6
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-09-29 08:10:08 -0300

    Dia 29/09

--- JS/Back-end/JS_6/2708-node-lib-md-arquivos-iniciais/index.js
+++ JS/Back-end/JS_6/2708-node-lib-md-arquivos-iniciais/index.js
@@ -1,6 +1,29 @@
 import chalk from "chalk";
+import fs, { readFile } from "fs"
 //const chalk = require('chalk')
 
-console.log(chalk.blue('Oi'));
+function trataErro(erro) {
+    console.log(erro)
+    throw new Error(chalk.red(erro.code, 'Arquivo inválido'))
+}
 
-console.log('olá mundo');
+function pegaArquivo(caminhoArquivo) { 
+    const enconding = 'utf-8'
+    
+//function pegaArquivo(caminhoArquivo) {
+//    const enconding = 'utf-8'
+//    fs.promises.readFile(caminhoArquivo, enconding)
+//    .then((texto) => console.log(chalk.green(texto)))
+//    .catch(trataErro) 
+//}
+//function pegaArquivo(caminhoArquivo) {
+//    const enconding = 'utf-8'
+//    fs.readFile(caminhoArquivo, enconding, (erro, texto) => {
+//        if (erro) {
+//            trataErro(erro)
+//        } else
+//        console.log(chalk.green(texto))
+//    })
+//}
+
+pegaArquivo('./arquivos/texto.md');
