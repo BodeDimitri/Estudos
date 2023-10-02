9515ef1e9839105289a0cfae8febfacbe33ce632
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-10-02 17:54:59 -0300

    Dia 02/10

--- JS/Back-end/JS_6/2708-node-lib-md-arquivos-iniciais/src/cli.js
+++ JS/Back-end/JS_6/2708-node-lib-md-arquivos-iniciais/src/cli.js
@@ -1,12 +1,39 @@
 import chalk from "chalk";
 import fs from "fs";
 import pegaArquivo from "./index.js";
+import { error } from "console";
 
 const caminho = process.argv
 
+function imprimeLista(resultado, identificador = '' ) {
+    console.log(chalk.yellow('lista de links'),
+    chalk.black.bgGreen(identificador),
+    resultado)
+}
+
 async function processaTexto(argumentos) {
-    const resultado = await pegaArquivo(argumentos[2])
-    console.log(chalk.yellow('lista de links'), resultado)
+    const caminho = argumentos[2];
+
+    try {
+        fs.lstatSync(caminho);
+    } catch(erro) {
+        if (erro.code === 'ENOENT') {
+            console.log('Arquivo não encontrado')
+            return
+        }
+    }
+
+    if(fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(argumentos[2])
+        imprimeLista(resultado) 
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho)
+         arquivos.forEach(async (nomeDeArquivos) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivos}`)
+            console.log(`${caminho}/${nomeDeArquivos}`)
+            imprimeLista(lista, nomeDeArquivos) 
+        })
+    }
 }
 
 processaTexto(caminho)
