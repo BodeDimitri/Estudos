456ba2c8b936a95115b8415dafd6cb7431f1fff6
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-10-18 18:58:32 -0300

    Eu sei, tem error, eu vou atualizar amanha

--- TS/typescript-curso-1-arquivos-iniciais/app/models/models/negociacao.ts
+++ TS/typescript-curso-1-arquivos-iniciais/app/models/models/negociacao.ts
@@ -1,26 +1,26 @@
 export default class Negociacao {
-    #data;
-    #quantidade;
-    #valor;
-    constructor(data, quantidade, valor) {
-        this.#data = data
-        this.#quantidade = quantidade
-        this.#valor = valor
+    private _data: Date;
+    private _quantidade: Number;
+    private _valor: Number;
+    constructor(data: Date, quantidade: Number, valor: Number) {
+        this._data = data
+        this._quantidade = quantidade
+        this._valor = valor
     }
 
     get data() {
-        return this.#data
+        return this._data
     }
 
     get quantidade() {
-        return this.#quantidade
+        return this._quantidade
     }
 
     get valor() {
-        return this.#valor
+        return this._valor
     }
 
     get volume() {
-        return this.#quantidade * this.#valor
+        return this._quantidade * this._valor
     }
 }
