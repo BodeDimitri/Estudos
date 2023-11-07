388bda789e86003818e258109571f296aa5bf269
Diego Sinhorini Albino <122684600+BodeDimitri@users.noreply.github.com>
2023-11-07 16:10:53 -0300

    Dia 07/11

---
+++ IaC/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-016485166ec7fa705"
+  instance_type = "t4g.small"
+
+  tags = {
+    Name = "primeira-instancia-terraform"
+  }
+}
