8562cc36c69191fa11c07b38936e81c419ea37c1
Diego Sinhorini Albino <diegocesaralbino@gmail.com>
2023-11-14 16:35:51 -0300

    Dia 14/11

--- IaC/main.tf
+++ IaC/main.tf
@@ -18,6 +18,12 @@ resource "aws_instance" "app_server" {
   ami           = "ami-016485166ec7fa705"
   instance_type = "t4g.small"
   key_name = "IaC-curso"
+  #user_data = <<-EOF
+  #           #!bin/bash
+  #           cd home/ubuntu
+  #           echo "<h1>feito pelo user data do terraform</h1>" > index.html
+  #           nohop busybox httpd -f -p 8080
+  #              EOF
   tags = {
     Name = "primeira-instancia-terraform"
   }
